c2c378b9dfef9c7f67d1940c6072f3ca816fbf39
silent0clown <cheng_xiaoyu@163.com>
2023-11-08 01:01:04 +0000

    licurl

--- libcurl/ReadMe.md
+++ libcurl/ReadMe.md
@@ -1,7 +1,18 @@
 # 1、下载安装libcurl
 ```bash
 git clone https://github.com/curl/curl.git
+tar -zxvf curl.8.4
+cd curl.8.4
+./configure --prefix=/usr/local/curl --with-openssl
+make
+make install
 ```
+# 2、编译test_libcurl.c
+```bash
+gcc test_libcurl.c -o proc_test -I /usr/local/curl/include/ -L /usr/local/curl/lib/ -lcurl
+```
+
+# libcurl是下载http的三方库
 
 [HOW TO USE](https://curl.se/docs/manpage.html)
 
---
+++ libcurl/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+size_t writeFunc(void *ptr, size_t size, size_t memb, void *userdata) {
+    printf("writeFunc...\n");
+}
+
+/* * url      : download url
+   # filename : download file to save path
+*/
+int curl_download(const char *url, const char *filename) {
+    CURL *curl = curl_easy_init();    // init
+    curl_easy_setopt(curl, CURLOPT_URL, url);  // connect
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunc);   // downdload callback
+
+    CURLcode res = curl_easy_perform(curl);         // process
+    if(res != CURLM_OK) {
+        printf("res %d\n", res);
+    }
+    curl_easy_cleanup(curl);      // disconnect
+
+    return 0;
+}
+
+// int main(int argc, char *argv[]) {
+//     curl_download(argv[1], argv[2]);
+int main() {
+    char *url = "www.baidu.com";
+    char *filename = "./test.html";
+    curl_download(url, filename);
+}
